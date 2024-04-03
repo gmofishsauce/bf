d3461d1033422f77d220d54addf0f2d7e9769216
gmofishsauce <pdxjjb@gmail.com>
2024-04-03 13:33:36 -0700

    Much cleanup. Added a proof concept preprocessor that converts the common loop '[-]' (set data[p] to 0) into an internal opcode that is printed as 'clear' at runtime. Done for now.

--- bfd.c
+++ bfd.c
@@ -5,21 +5,30 @@
 #include <stdlib.h>
 #include <string.h>
 
+// BF state
 int  p = 0;
-char data[64*1024], prog[64*1024];
+char data[64*1024];
+char prog[64*1024];
 
-// Variables for printing
+// Variables for printing. The first 14 locations in the array data[] are named
+// A, B, C, etc. The rest of the locations are named data[n].
 const char lowVars[] = "ABCDEFGHIJKLMN";
+int incDecCount = 0;
 int nIndent = 0;
 char indentBuf[60];
 
-// Buffer output to prevent it from getting
-// jumbled with the extensive debug output.
+// Buffer output to prevent it from getting jumbled with the extensive debug output.
+// Output is issued when a newline is seen or a , (comma) operator ("getchar") is
+// encountered (because the buffered output might be a prompt). Output is marked
+// with an identifying string, "[OUTPUT]", to support grep.
 char outputBuf[80];
 int  outbufIndex;
 
 #define SPACE ' '
 
+// Print the proper indent's worth of spaces by copying up to "max" spaces
+// into the spaces buffer, nul terminating the buffer, and returning it.
+// The "-1" on the end variable leaves space for the terminating nul.
 char *indent() {
     int n = 2*nIndent;
     char *end = (n >= sizeof(indentBuf)-1) ? indentBuf+sizeof(indentBuf)-1 : indentBuf+n;
@@ -31,76 +40,77 @@ char *indent() {
     return indentBuf;
 }
 
-void printIncDec(int p, int ct) {
+// If the accumulated count of increments and decrements is not 0,
+// print it with the proper indent and clear the count.
+void printIncDec() {
+    if (incDecCount == 0) {
+        return;
+    }
     if (p < sizeof(lowVars)) {
         printf("%s%c %s %d // (%c == %d)\n", indent(),
-            lowVars[p], (ct >= 0) ? "+=" : "-=", abs(ct), lowVars[p], data[p]);
+            lowVars[p], (incDecCount >= 0) ? "+=" : "-=", abs(incDecCount), lowVars[p], data[p]);
     } else {
         printf("%sdata[%d] %s %d // (data[%d] = %d)\n", indent(),
-            p, (ct >= 0) ? "+=" : "-=", abs(ct), p, data[p]);
+            p, (incDecCount >= 0) ? "+=" : "-=", abs(incDecCount), p, data[p]);
+    }
+    incDecCount = 0;
+}
+
+// It's difficult to find the output from the BF program buried in the massive
+// trace output. To help, we buffer output until either a newline or GET (comma)
+// is seen and then push the output preceded by string "[OUTPUT]". This helps
+// with grep.
+void flush() {
+    if (outbufIndex > 0) {
+        printf("[OUTPUT] %s", outputBuf);
+        fflush(stdout);
+        outbufIndex = 0;
+        memset(outputBuf, 0, sizeof(outputBuf));
     }
 }
 
 void interpret(char *instrPtr)
 {
-    int incCount = 0;
     printf("%s{\n", indent());
     nIndent++;
 
     while( *instrPtr ) {
         switch(*instrPtr++) {
         case '<':
-            if (incCount != 0) {
-                printIncDec(p, incCount);
-                incCount = 0;
-            }
+            printIncDec();
             p--;
             break;
         case '>':
-            if (incCount != 0) {
-                printIncDec(p, incCount);
-                incCount = 0;
-            }
+            printIncDec();
             p++;
             break;
         case '+':
-            incCount++;
+            incDecCount++;
             data[p]++;
             break;
         case '-':
-            incCount--;
+            incDecCount--;
             data[p]--;
             break;
         case '.':
-            if (incCount != 0) {
-                printIncDec(p, incCount);
-                incCount = 0;
-            }
+            printIncDec();
             if (data[p] == '\n') {
                 printf("%sPUT(%s)\n", indent(), "\\n");
             } else {
                 printf("%sPUT(%c)\n", indent(), data[p]);
             }
+            // We buffer output to make it easier to find
             outputBuf[outbufIndex++] = data[p];
             if (outbufIndex >= sizeof(outputBuf) || data[p] == '\n') {
-                printf("[OUTPUT] %s", outputBuf);
-                fflush(stdout);
-                outbufIndex = 0;
-                memset(outputBuf, 0, sizeof(outputBuf));
+                flush();
             }
             break;
         case ',':
-            if (incCount != 0) {
-                printIncDec(p, incCount);
-                incCount = 0;
-            }
+            printIncDec();
             // If there's a buffered prompt,
             // flush it before we get a char.
             if (outbufIndex != 0) {
-                printf("[OUTPUT] %s", outputBuf);
-                fflush(stdout);
-                outbufIndex = 0;
-                memset(outputBuf, 0, sizeof(outputBuf));
+                flush();
             }
             data[p]=getchar();
             if (p < sizeof(lowVars)) {
@@ -112,10 +122,7 @@ void interpret(char *instrPtr)
             break;
         case '[':
         {
-            if (incCount != 0) {
-                printIncDec(p, incCount);
-                incCount = 0;
-            }
+            printIncDec();
             if (p < sizeof(lowVars)) {
                 printf("%swhile(%c != 0) // %c == %d\n", indent(), lowVars[p], lowVars[p], data[p]);
             } else {
@@ -145,12 +152,25 @@ void interpret(char *instrPtr)
         case ']':
             printf("unbalanced bracket near %ld\n", (instrPtr - prog));
             exit(0);
+        case -2:
+            if (p < sizeof(lowVars)) {
+                printf("%sclear %c // %c == 0\n", indent(), lowVars[p], lowVars[p]);
+            } else {
+                printf("%sclear data[%d] // data[%d] == 0)\n", indent(), p, p);
+            }
+            data[p] = 0;
+            break;
+        case -1:
+            break;
+        default:
+            if (instrPtr[-1] >= 0) {
+                // only print ASCII chars
+                putchar(*(instrPtr-1));        
+            }
+            break;
         }
     }
-    if (incCount != 0) {
-        printIncDec(p, incCount);
-        incCount = 0;
-    }
+    printIncDec();
     nIndent--;
     printf("%s}\n", indent());
 }
@@ -171,7 +191,12 @@ int main(int ac, char *av[])
     // Load the program from the file with a terminating nul char
     // (binary 0). The comments are loaded along with the code,
     // then ignored by the interpreter. The "-1" on the length
-    // check accommodates the terminating nul.
+    // check accommodates the terminating nul. Note that trying
+    // to load a binary file having bytes >= 0x80 unsigned will
+    // not work; the first such byte is treated as end of file.
+    // The proof of concept condenser takes advantage of this by
+    // using bytes between unsigned 0x80 and 0xFF as compressed
+    // opcodes which are understood by the interpreter.
 
     int nOp = 0;
     char b;
@@ -180,10 +205,19 @@ int main(int ac, char *av[])
             printf("Program is too long.");
             exit(2);
         }
-        prog[nOp++] = b;
+        // This is a proof of concept condenser for the sequence [-]
+        // This is equivalent to "set the byte at the pointer to 0".
+        if (b == ']' && nOp >= 2 && prog[nOp-1] == '-' && prog[nOp-2] == '[') {
+            prog[nOp-2] = 0xFE; // "CLEAR data[p]"
+            prog[nOp-1] = 0xFF; // noop
+            prog[nOp++] = 0xFF; // noop
+        } else {
+            prog[nOp++] = b;
+        }
     }
     prog[nOp++] = 0;
-
+    // We could implement SMBF (self-modifying brainfuck) by setting the data
+    // pointer to nOp here and having just one array to hold code and data. TBD.
     interpret(prog);
 }
 
